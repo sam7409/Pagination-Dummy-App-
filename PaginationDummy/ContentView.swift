2503172b82041b6056bfcf164d75f64053c51590
Neeshu Kumar <neeshu.kumar@simplyentertainingapps.com>
2024-07-26 19:08:40 +0530

    Initial Commit

---
+++ PaginationDummy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PaginationDummy
+//
+//  Created by Neeshu Kumar on 26/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PaginationDummy/PaginationDummyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaginationDummyApp.swift
+//  PaginationDummy
+//
+//  Created by Neeshu Kumar on 26/07/24.
+//
+
+import SwiftUI
+
+@main
+struct PaginationDummyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
